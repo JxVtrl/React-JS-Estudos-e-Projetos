d6b093e2ef1545991af6a5ea669b952f12a29364
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-07-13 17:40:08 -0300

    :ok_hand: Studying...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './componentes/App';
 import App2 from './componentes/App2';
 import App3 from './componentes/App3';
 import App4 from './componentes/App4';
-import listas from './componentes/Listas';
+import {Lista, Item} from './componentes/Lista';
 
 
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +12,15 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   // Nome do arquivo em que se quer renderizar a tela dentro da tag React.StrictMode <nome do arquivo>
   <React.StrictMode>
-    <listas></listas>
+    <Lista>
+        <Item key="999" id="999" feito={false}>
+          Teste Item
+        </Item>
+        <Item key="998" id="998" feito={true}>
+          Teste Item 2
+        </Item>
+        Texto que não deve aparecer
+    </Lista>
   </React.StrictMode>,
   document.getElementById('root')
 );
