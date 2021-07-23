8d094e8b8fc3271eb700b63c8ae369a3aa32610c
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-07-23 19:01:40 -0300

    :tada: dunno

---
+++ src/componentes/Formulario.js
@@ -0,0 +1,78 @@
+import { logRoles } from '@testing-library/react';
+import React from 'react';
+
+class Formulario extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            nome: '',
+            linguagem: 'JavaScript',
+            profissao: 'programador',
+            dedico: true,
+            bio: ''
+        }
+        this.handleSubmit = (event) =>{
+            event.preventDefault()
+            console.log(this.state)
+        }
+
+        this.changeName = (event) =>{
+            this.setState({nome: event.target.value})
+        }
+
+        this.changeSelect = (event) =>{
+            this.setState({linguagem: event.target.value})
+        }
+
+        this.changeRadio = (event) =>{
+            this.setState({profissao: event.target.value})
+        }
+
+        this.changeCheckbox = (event) =>{
+            this.setState({dedico: event.target.checked})
+        }
+
+        this.changeBio = (event) =>{
+            this.setState({bio: event.target.value})
+        }
+    }
+
+    render(){
+        return(
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Nome: <input type="text" value={this.state.nome} onChange={this.changeName}></input>
+                    </label>
+                    <br/>
+                    <label>
+                        Linguagem favorita:
+                        <select value={this.state.linguagem} onChange={this.changeSelect}>
+                            <option>JavaScript</option>
+                            <option>Python</option>
+                            <option>C++</option>
+                        </select>
+                    </label>
+                    <br/>
+                    <label>
+                        Sou:
+                        <input type="radio" checked={this.state.profissao == 'programador'} onChange={this.changeRadio} value="programador"/>Programador
+                        <input type="radio" checked={this.state.profissao == 'estudante'} onChange={this.changeRadio} value="estudante"/>Estudante
+                    </label>
+                     <br />
+                    <label>
+                        <input type="checkbox" checked={this.state.dedico} onChange={this.changeCheckbox} /> Dedico 8 horas semanais a estudos
+                    </label>
+                    <br/>
+                    <label>
+                        Bio:<textarea cols="50" value={this.state.bio} onChange={this.changeBio} />
+                    </label>
+                    <br />
+                    <input type="submit" value="Salvar"/>
+                </form>
+            </>
+        )  
+    }
+}
+
+export default Formulario;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import UseState from './componentes/UseState';
 import Efeitos from './componentes/Efeitos'
+=======
+import App from './componentes/App';
+import App2 from './componentes/App2';
+import App3 from './componentes/App3';
+import App4 from './componentes/App4';
+import {Lista, Item} from './componentes/Lista';
+import Formulario from './componentes/Formulario'
+
+>>>>>>> Stashed changes
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <Efeitos></Efeitos>
+=======
+    <Formulario></Formulario>
+>>>>>>> Stashed changes
   </React.StrictMode>,
   document.getElementById('root')
 );
